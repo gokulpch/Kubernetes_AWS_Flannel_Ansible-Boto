7b652addcb06b56865d88f2c6b33f1f7e284cd74
gokulpch <gokulpch@bu.edu>
2017-06-12 07:51:32 +0000

    Changes to Dashboard

--- ansible/utils/create_K8S_dashboard.sh
+++ ansible/utils/create_K8S_dashboard.sh
@@ -4,10 +4,13 @@
 ########## Deploy Kubernetes Dashboard                                 #####
 ######
 ############################################################################
+echo "Pulling Kubernetes-Demo Repo............"
+git clone https://github.com/gokulpch/Kubernetes_Demo_YAML.git
 URL="https://s3-us-west-2.amazonaws.com/contrail-docker-images/K8Sdashboard.yaml"
 echo $(date) 'Running wget...'
 wget "${URL}"
 echo 'Done Copying File.............'
 sleep 60s
-kubectl create -f K8Sdashboard.yaml
+kubectl --kubeconfig /root/admin.conf create -f /root/K8Sdashboard.yaml
+#cd /root/.; kubectl create -f K8Sdashboard.yaml
 echo 'Kubernetes Dashboard Created.............'
--- create_cluster.sh
+++ create_cluster.sh
@@ -4,5 +4,15 @@
 ###################Ansible playbook to initiate the cluster#################
 ######
 ############################################################################
-echo "Running ansible to create K8S cluster................."
-ansible-playbook ./ansible/create_cluster.yml -v
+printf "Running ansible to create K8S cluster"
+for i in `seq 1 5`;do echo;done
+printf "Opening a new screen session......."
+for i in `seq 1 5`;do echo;done
+tmux new -d -s K8S-Install 'ANSIBLE_LOG_PATH="/root/install.log" ansible-playbook ./ansible/create_cluster.yml -v'
+tmux detach -s K8S-Install
+#ansible-playbook ./ansible/create_cluster.yml -v
+for i in `seq 1 5`;do echo;done
+echo "To attach use 'tmux attach -t K8S-Install'"
+for i in `seq 1 2`;do echo;done
+echo "To detach(leave the session running) use 'Ctrl-b d'"
+for i in `seq 1 5`;do echo;done
